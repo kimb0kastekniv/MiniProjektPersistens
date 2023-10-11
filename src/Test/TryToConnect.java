accd71725d40eb6ba2bc5f58bfbaf2c6d3bd6aa7
kimb0kastekniv <10197768@ucn.dk>
2023-10-11 14:11:25 +0200

    DBConnection

---
+++ src/Test/TryToConnect.java
@@ -0,0 +1,40 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import dblayer.DBConnection;
+
+class TryToConnect {
+    
+    private static DBConnection dbConnection;
+    private static Connection connection;
+    
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        dbConnection = DBConnection.getInstance();
+        connection = dbConnection.getConnection();
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception {
+        dbConnection.disconnect();
+    }
+
+    @Test
+    void testConnection() {
+        try {
+            assertFalse(connection.isClosed());
+            System.out.println("Connection is open!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail("Connection is closed!");
+        }
+    }
+}
--- src/dblayer/DBConnection.java
+++ src/dblayer/DBConnection.java
@@ -1,44 +1,129 @@
 package dblayer;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBConnection {
-	private Connection connection = null;
-	private static DBConnection dbConnection;
-	
-	private static final String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static final String dbName = "DMA-CSD-V23_10511081";
-	//private static final String serverAddress = "localhost";
+	// Constants used to get access to the database
+
+	// private static final String driver = "jdbc:sqlserver://localhost:1433";
 	private static final String serverAddress = "hildur.ucn.dk";
-	private static final int    serverPort = 1433;
-	private static final String userName = "DMA-CSD-V23_10511081";
-	private static final String password = "Password1!";
-	
-	private DBConnection() {
-		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s;encrypt=false", 
-				serverAddress, serverPort, dbName, userName, password);
+	private static final String databaseName = "DMA-CSD-V23_10511081";
+
+	private static String userName = "DMA-CSD-V23_10511081";
+	private static String password = "Password1!";
+	private static String encryption = "encrypt=false";
+
+	private DatabaseMetaData dma;
+	private static Connection con;
+
+	// an instance of the class is generated
+	private static DBConnection instance = null;
+
+	// the constructor is private to ensure that only one object of this class is
+	// created
+
+	public static void main(String[] args) throws SQLException {
 		try {
-			Class.forName(driverClass);
-			connection = DriverManager.getConnection(connectionString);
-		} catch (ClassNotFoundException e) {
-			System.err.println("Could not load JDBC driver");
-			e.printStackTrace();
+			DBConnection dbConnection = DBConnection.getInstance();
+			Connection connection = dbConnection.getDBcon();
+
+			if (dbConnection.instanceIsNull()) {
+				System.out.println("Failed to get instance of DBConnection.");
+			} else if (dbConnection.getOpenStatus()) {
+				System.out.println("Connected to the database!");
+			} else {
+				System.out.println("Failed to connect to the database.");
+			}
+
+			dbConnection.closeConnection();
 		} catch (SQLException e) {
-			System.err.println("Could not connect to database " + dbName + "@" + serverAddress + ":" + serverPort + " as user " + userName + " using password ******");
-			System.out.println("Connection string was: " + connectionString.substring(0, connectionString.length() - password.length()) + "....");
 			e.printStackTrace();
 		}
 	}
-	
-	public static DBConnection getInstance() {
-		if(dbConnection == null) {
-			dbConnection = new DBConnection();
+
+	private DBConnection() {
+		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s;encrypt=false",
+				serverAddress, serverPort, dbName, userName, password);
+		System.out.println("URL: " + url);
+
+		try {
+			// load of driver
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			System.out.println("Driver class loaded ok");
+
+		} catch (Exception e) {
+			System.out.println("Cannot find the driver");
+			System.out.println(e.getMessage());
 		}
-		return dbConnection;
+		try {
+			// connection to the database
+			con = DriverManager.getConnection(url);
+			con.setAutoCommit(true);
+			dma = con.getMetaData(); // get meta data
+			System.out.println("Connection to " + dma.getURL());
+			System.out.println("Driver " + dma.getDriverName());
+			System.out.println("Database product name " + dma.getDatabaseProductName());
+		} // end try
+		catch (Exception e) {
+			System.out.println("Problems with the connection to the database:");
+			System.out.println(e.getMessage());
+			System.out.println(url);
+		} // end catch
+	}// end constructor
+
+	// closeDb: closes the connection to the database
+	public static void closeConnection() {
+		try {
+			con.close();
+			instance = null;
+			System.out.println("The connection is closed");
+		} catch (Exception e) {
+			System.out.println("Error trying to close the database " + e.getMessage());
+		}
+	}// end closeDB
+
+	// getDBcon: returns the singleton instance of the DB connection
+	public Connection getDBcon() {
+		return con;
 	}
+
+	// getDBcon: returns the singleton instance of the DB connection
+	public static boolean instanceIsNull() {
+		return (instance == null);
+	}
+
+	// this method is used to get the instance of the connection
+	public static DBConnection getInstance() {
+		if (instance == null) {
+			instance = new DBConnection();
+		}
+		return instance;
+	}
+
+	public static boolean getOpenStatus() {
+		boolean isOpen = false;
+		try {
+			isOpen = (!con.isClosed());
+		} catch (Exception sclExc) {
+			isOpen = false;
+		}
+		return isOpen;
+	}
+
+	public Connection getConnection() {
+		return con;
+	}
+
+	public void disconnect() {
+		try {
+			con.close();
+			System.out.println("The connection is closed");
+		} catch (SQLException e) {
+			System.out.println("Error trying to close the database " + e.getMessage());
+		}
+	}
+
 }
