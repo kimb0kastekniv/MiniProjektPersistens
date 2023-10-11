0de53b6d725454b1034f738abf12a9c899e69854
kimb0kastekniv <10197768@ucn.dk>
2023-10-11 15:05:45 +0200

    DBConnection rettet

---
+++ src/controller/DataAccessException.java
@@ -0,0 +1,12 @@
+package controller;
+
+
+
+
+public class DataAccessException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public DataAccessException(String message, Throwable e) {
+		super(message, e);
+	}
+}
--- src/dblayer/DBConnection.java
+++ src/dblayer/DBConnection.java
@@ -1,129 +1,62 @@
 package dblayer;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import controller.DataAccessException;
+
 
 public class DBConnection {
-	// Constants used to get access to the database
+	private Connection connection = null;
+	private static DBConnection dbConnection;
 
-	// private static final String driver = "jdbc:sqlserver://localhost:1433";
+	private static final String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static final String dbName = "DMA-CSD-V23_10511081";
 	private static final String serverAddress = "hildur.ucn.dk";
-	private static final String databaseName = "DMA-CSD-V23_10511081";
+	
 
-	private static String userName = "DMA-CSD-V23_10511081";
-	private static String password = "Password1!";
-	private static String encryption = "encrypt=false";
+	private static final String userName = "DMA-CSD-V23_10511081";
+	private static final String password = "Password1!";
 
-	private DatabaseMetaData dma;
-	private static Connection con;
-
-	// an instance of the class is generated
-	private static DBConnection instance = null;
-
-	// the constructor is private to ensure that only one object of this class is
-	// created
-
-	public static void main(String[] args) throws SQLException {
-		try {
-			DBConnection dbConnection = DBConnection.getInstance();
-			Connection connection = dbConnection.getDBcon();
-
-			if (dbConnection.instanceIsNull()) {
-				System.out.println("Failed to get instance of DBConnection.");
-			} else if (dbConnection.getOpenStatus()) {
-				System.out.println("Connected to the database!");
-			} else {
-				System.out.println("Failed to connect to the database.");
-			}
-
-			dbConnection.closeConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private DBConnection() {
+	private DBConnection() throws DataAccessException {
+		
 		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s;encrypt=false",
-				serverAddress, serverPort, dbName, userName, password);
-		System.out.println("URL: " + url);
-
+				serverAddress, dbName, userName, password);
 		try {
-			// load of driver
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			System.out.println("Driver class loaded ok");
-
-		} catch (Exception e) {
-			System.out.println("Cannot find the driver");
-			System.out.println(e.getMessage());
+			Class.forName(driverClass);
+			connection = DriverManager.getConnection(connectionString);
+		} catch (ClassNotFoundException e) {
+			throw new DataAccessException("Missing JDBC driver", e);
+			
+		} catch (SQLException e) {
+			throw new DataAccessException(String.format("Could not connect to database %s@%s:%d user %s", dbName,
+					serverAddress, userName), e);
+			
 		}
-		try {
-			// connection to the database
-			con = DriverManager.getConnection(url);
-			con.setAutoCommit(true);
-			dma = con.getMetaData(); // get meta data
-			System.out.println("Connection to " + dma.getURL());
-			System.out.println("Driver " + dma.getDriverName());
-			System.out.println("Database product name " + dma.getDatabaseProductName());
-		} // end try
-		catch (Exception e) {
-			System.out.println("Problems with the connection to the database:");
-			System.out.println(e.getMessage());
-			System.out.println(url);
-		} // end catch
-	}// end constructor
-
-	// closeDb: closes the connection to the database
-	public static void closeConnection() {
-		try {
-			con.close();
-			instance = null;
-			System.out.println("The connection is closed");
-		} catch (Exception e) {
-			System.out.println("Error trying to close the database " + e.getMessage());
-		}
-	}// end closeDB
-
-	// getDBcon: returns the singleton instance of the DB connection
-	public Connection getDBcon() {
-		return con;
 	}
 
-	// getDBcon: returns the singleton instance of the DB connection
-	public static boolean instanceIsNull() {
-		return (instance == null);
+	public static synchronized DBConnection getInstance() throws DataAccessException {
+		if (dbConnection == null) {
+			dbConnection = new DBConnection();
+		}
+		return dbConnection;
 	}
 
-	// this method is used to get the instance of the connection
-	public static DBConnection getInstance() {
-		if (instance == null) {
-			instance = new DBConnection();
-		}
-		return instance;
-	}
-
-	public static boolean getOpenStatus() {
-		boolean isOpen = false;
-		try {
-			isOpen = (!con.isClosed());
-		} catch (Exception sclExc) {
-			isOpen = false;
-		}
-		return isOpen;
-	}
+	
 
 	public Connection getConnection() {
-		return con;
+		return connection;
 	}
 
 	public void disconnect() {
 		try {
-			con.close();
-			System.out.println("The connection is closed");
+			connection.close();
 		} catch (SQLException e) {
-			System.out.println("Error trying to close the database " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
-
 }
