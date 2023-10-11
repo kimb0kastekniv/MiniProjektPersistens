98ec6bcde1c75cec279664c27d8787a5e23df9d1
ShawnLohse <10478740@ucn.dk>
2023-10-11 12:22:32 +0200

    Country created

--- src/model/Country.java
+++ src/model/Country.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Country {
+	private int countryId;
+	private String name;
 
+	public Country(int countryId, String name) {
+		this.countryId = countryId;
+		this.name = name;
+	}
+
+	public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
