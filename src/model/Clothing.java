0e7afb78cf3e3cbb87da12468056e78d03cdfbe6
ShawnLohse <10478740@ucn.dk>
2023-10-11 13:44:02 +0200

    Create clothing

--- src/model/Clothing.java
+++ src/model/Clothing.java
@@ -1,5 +1,43 @@
 package model;
 
-public class Clothing {
+public class Clothing extends Product {
+	private int clId;
+	private String size;
+	private String colour;
 
+	public Clothing(int productId, String name, double purchasePrice, double salesPrice, int minStock,
+			Supplier supplierId, Country countryId, int type, int clId, String size, String colour) {
+		// Call the constructor of the superclass (Product) to initialize inherited
+		// fields
+		super(productId, name, purchasePrice, salesPrice, minStock, supplierId, countryId, type);
+		// Initialize the fields specific to Clothing
+		this.clId = clId;
+		this.size = size;
+		this.colour = colour;
+	}
+
+	// Getters and Setters
+	public int getClId() {
+		return clId;
+	}
+
+	public void setClId(int clId) {
+		this.clId = clId;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
 }
