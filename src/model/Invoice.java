613286213960e526601bb1dbe89491b2a03fdfa8
ShawnLohse <10478740@ucn.dk>
2023-10-11 13:44:28 +0200

    Create invoice

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -1,5 +1,40 @@
 package model;
 
-public class Invoice {
+import java.util.Date;
 
+public class Invoice {
+	private int invoiceId;
+	private int paymentDate;
+	private int amount;
+
+	public Invoice(int invoiceId, int paymentDate, int amount) {
+		this.invoiceId = invoiceId;
+		this.paymentDate = paymentDate;
+		this.amount = amount;
+	}
+
+	// Getters and Setters
+	public int getInvoiceId() {
+		return invoiceId;
+	}
+
+	public void setInvoiceId(int invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+
+	public int getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(int paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
 }
