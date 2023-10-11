813d10b5461dfb463bba897590584ef11856a3ca
ShawnLohse <10478740@ucn.dk>
2023-10-11 13:45:04 +0200

    Create SaleOrderLine

--- src/model/SaleOrderLine.java
+++ src/model/SaleOrderLine.java
@@ -1,5 +1,41 @@
 package model;
 
 public class SaleOrderLine {
+	private int saleOrderId; // Composite primary key with product_id
+	private int productId; // Composite primary key with saleOrderId
+	private int quantity;
+	private double salePrice;
 
+	// Getters and Setters
+	public int getSaleOrderId() {
+		return saleOrderId;
+	}
+
+	public void setSaleOrderId(int saleOrderId) {
+		this.saleOrderId = saleOrderId;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getSalePrice() {
+		return salePrice;
+	}
+
+	public void setSalePrice(double salePrice) {
+		this.salePrice = salePrice;
+	}
 }
