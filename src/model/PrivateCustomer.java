259f82283a3c8d690e9a5bdc3f4c8f716565feae
ShawnLohse <10478740@ucn.dk>
2023-10-11 12:22:56 +0200

    private customer created

--- src/model/PrivateCustomer.java
+++ src/model/PrivateCustomer.java
@@ -1,5 +1,49 @@
 package model;
 
-public class PrivateCustomer {
+public class PrivateCustomer extends Customer {
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private int pcId;
 
+	public PrivateCustomer(String firstName, String middleName, String lastName, int pcId, int customerId,
+			String streetName, int streetNo, int zipcode, int countryId, int phone, String email, int customerType) {
+		super(customerId, streetName, streetNo, zipcode, countryId, phone, email, customerType);
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.pcId = pcId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getPcId() {
+		return pcId;
+	}
+
+	public void setPcId(int pcId) {
+		this.pcId = pcId;
+	}
 }
