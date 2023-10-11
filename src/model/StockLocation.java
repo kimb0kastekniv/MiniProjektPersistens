c9bd67a9e09cf04e26d630934875b8c1cc8e8374
ShawnLohse <10478740@ucn.dk>
2023-10-11 14:07:00 +0200

    Create StockLocation

--- src/model/StockLocation.java
+++ src/model/StockLocation.java
@@ -1,5 +1,58 @@
 package model;
 
 public class StockLocation {
+	private int stockLocationId;
+	private String streetName;
+	private int streetNo;
+	private ZipCodeCity zipcode;
+	private Country countryId;
 
+	public StockLocation(int stockLocationId, String streetName, int streetNo, ZipCodeCity zipcode, Country countryId) {
+		this.stockLocationId = stockLocationId;
+		this.streetName = streetName;
+		this.streetNo = streetNo;
+		this.zipcode = zipcode;
+		this.countryId = countryId;
+	}
+
+	// Getters and Setters
+	public int getStockLocationId() {
+		return stockLocationId;
+	}
+
+	public void setStockLocationId(int stockLocationId) {
+		this.stockLocationId = stockLocationId;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public int getStreetNo() {
+		return streetNo;
+	}
+
+	public void setStreetNo(int streetNo) {
+		this.streetNo = streetNo;
+	}
+
+	public ZipCodeCity getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(ZipCodeCity zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public Country getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Country countryId) {
+		this.countryId = countryId;
+	}
 }
