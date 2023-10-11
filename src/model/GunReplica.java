aa29e3b307d760c7d02b1412682b6f0c1abc0272
ShawnLohse <10478740@ucn.dk>
2023-10-11 14:06:14 +0200

    Create GunReplica

--- src/model/GunReplica.java
+++ src/model/GunReplica.java
@@ -1,5 +1,41 @@
 package model;
 
-public class GunReplica {
+public class GunReplica extends Product {
+	private double calibre;
+	private String material;
+	private int grId;
 
+	public GunReplica(int productId, String name, double purchasePrice, double salesPrice, int minStock,
+			Supplier supplierId, Country countryId, int type, double calibre, String material, int grId) {
+		super(productId, name, purchasePrice, salesPrice, minStock, supplierId, countryId, type);
+
+		this.calibre = calibre;
+		this.material = material;
+		this.grId = grId;
+	}
+
+	// Getters and Setters
+	public double getCalibre() {
+		return calibre;
+	}
+
+	public void setCalibre(double calibre) {
+		this.calibre = calibre;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public int getGrId() {
+		return grId;
+	}
+
+	public void setGrId(int grId) {
+		this.grId = grId;
+	}
 }
