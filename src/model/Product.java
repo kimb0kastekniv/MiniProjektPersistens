1316a368af988839772448aa8462bd362610ac26
ShawnLohse <10478740@ucn.dk>
2023-10-11 13:44:37 +0200

    Create product

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,89 @@
 package model;
 
-public class Product {
+public abstract class Product {
+	private int productId;
+	private String name;
+	private double purchasePrice;
+	private double salesPrice;
+	private int minStock;
+	private Supplier supplierId;
+	private Country countryId; // Foreign key to Country table
+	private int type;
 
+	public Product(int productId, String name, double purchasePrice, double salesPrice, int minStock,
+			Supplier supplierId, Country countryId, int type) {
+		this.productId = productId;
+		this.name = name;
+		this.purchasePrice = purchasePrice;
+		this.salesPrice = salesPrice;
+		this.minStock = minStock;
+		this.supplierId = supplierId;
+		this.countryId = countryId;
+		this.type = type;
+	}
+
+	// Getters and Setters
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public double getSalesPrice() {
+		return salesPrice;
+	}
+
+	public void setSalesPrice(double salesPrice) {
+		this.salesPrice = salesPrice;
+	}
+
+	public int getMinStock() {
+		return minStock;
+	}
+
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+
+	public Supplier getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(Supplier supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public Country getCountryId() {
+		return countryId;
+	}
+
+	public void setCountryId(Country countryId) {
+		this.countryId = countryId;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
 }
