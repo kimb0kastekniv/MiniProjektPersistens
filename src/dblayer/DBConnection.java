32d2b5f346ab3149b500257203c6a27ace165fcb
xdmurkch <10461252@ucn.dk>
2023-10-11 11:03:45 +0200

    add initial dbconnection code

--- src/dblayer/DBConnection.java
+++ src/dblayer/DBConnection.java
@@ -1,5 +1,44 @@
 package dblayer;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class DBConnection {
+	private Connection connection = null;
+	private static DBConnection dbConnection;
+	
+	private static final String driverClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static final String dbName = "DMA-CSD-V23_10511081";
+	//private static final String serverAddress = "localhost";
+	private static final String serverAddress = "hildur.ucn.dk";
+	private static final int    serverPort = 1433;
+	private static final String userName = "DMA-CSD-V23_10511081";
+	private static final String password = "Password1!";
+	
+	private DBConnection() {
+		String connectionString = String.format("jdbc:sqlserver://%s:%d;databaseName=%s;user=%s;password=%s;encrypt=false", 
+				serverAddress, serverPort, dbName, userName, password);
+		try {
+			Class.forName(driverClass);
+			connection = DriverManager.getConnection(connectionString);
+		} catch (ClassNotFoundException e) {
+			System.err.println("Could not load JDBC driver");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("Could not connect to database " + dbName + "@" + serverAddress + ":" + serverPort + " as user " + userName + " using password ******");
+			System.out.println("Connection string was: " + connectionString.substring(0, connectionString.length() - password.length()) + "....");
+			e.printStackTrace();
+		}
+	}
+	
+	public static DBConnection getInstance() {
+		if(dbConnection == null) {
+			dbConnection = new DBConnection();
+		}
+		return dbConnection;
+	}
 }
