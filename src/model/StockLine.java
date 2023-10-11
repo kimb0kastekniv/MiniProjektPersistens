ccbab427bb4e248ab6f2ee9e885722d1e3477d7a
ShawnLohse <10478740@ucn.dk>
2023-10-11 14:06:46 +0200

    Create StockLine

--- src/model/StockLine.java
+++ src/model/StockLine.java
@@ -1,5 +1,38 @@
 package model;
 
 public class StockLine {
+	private int numberInStock;
+	private StockLocation stockLocationId;
+	private Product productId;
 
+	public StockLine(int numberInStock, StockLocation stockLocationId, Product productId) {
+		this.numberInStock = numberInStock;
+		this.stockLocationId = stockLocationId;
+		this.productId = productId;
+	}
+
+	// Getters and Setters
+	public int getNumberInStock() {
+		return numberInStock;
+	}
+
+	public void setNumberInStock(int numberInStock) {
+		this.numberInStock = numberInStock;
+	}
+
+	public StockLocation getStockLocationId() {
+		return stockLocationId;
+	}
+
+	public void setStockLocationId(StockLocation stockLocationId) {
+		this.stockLocationId = stockLocationId;
+	}
+
+	public Product getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Product productId) {
+		this.productId = productId;
+	}
 }
