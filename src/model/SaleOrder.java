dcef9708b8340951ba6978ee81dfdc0b6ca76de7
ShawnLohse <10478740@ucn.dk>
2023-10-12 13:08:00 +0200

    Update SaleOrder.java

--- src/model/SaleOrder.java
+++ src/model/SaleOrder.java
@@ -10,41 +10,69 @@ public class SaleOrder {
 	private int deliveryDate;
 	private Invoice invoice; // Foreign key to Invoice table
 	private Customer customer; // Foreign key to Customer table
-	
+
 	private ArrayList<SaleOrderLine> saleOrderLine;
 	private Discount discount;
+	private ArrayList<Product> productsInOrder;
 
-
-	public SaleOrder(int saleOrderId, int date, double amount, boolean deliveryStatus, int deliveryDate, Invoice invoice, Customer customer) {
-        this.saleOrderId = saleOrderId;
-        this.date = date;
-        this.amount = amount;
-        this.deliveryStatus = deliveryStatus;
-        this.deliveryDate = deliveryDate;
-        this.invoice = invoice;
-        this.customer = customer;
-        saleOrderLine = new ArrayList<SaleOrderLine>();
-        discount = new Discount();
-    }
-	
-	public SaleOrder() {
-		 saleOrderLine = new ArrayList<>();
-	        discount = new Discount();
+	public SaleOrder(int saleOrderId, int date, double amount, boolean deliveryStatus, int deliveryDate,
+			Invoice invoice, Customer customer) {
+		this.saleOrderId = saleOrderId;
+		this.date = date;
+		this.amount = amount;
+		this.deliveryStatus = deliveryStatus;
+		this.deliveryDate = deliveryDate;
+		this.invoice = invoice;
+		this.customer = customer;
+		saleOrderLine = new ArrayList<SaleOrderLine>();
+		discount = new Discount();
+		productsInOrder = new ArrayList<>();
 	}
-	
+
+	public SaleOrder() {
+		saleOrderLine = new ArrayList<>();
+		discount = new Discount();
+	}
+
 	public void createSaleOrderLine(SaleOrder saleOrderId, Product productId, int quantity, double salePrice) {
-		SaleOrderLine sol = new SaleOrderLine(saleOrderId, productId, quantity, salePrice);
+		SaleOrderLine sol = new SaleOrderLine(this, productId, quantity, salePrice);
 		saleOrderLine.add(sol);
 	}
+
 	public double getAmountWithDiscount(double input) {
 		int customerType = customer.getCustomerType();
 		double saleOrderAmount = input;
 		double amount = discount.getAmountWithDiscount(customerType, saleOrderAmount);
 		return amount;
 	}
-	
-	
-	
+
+	public ArrayList<SaleOrderLine> getSaleOrderLines() {
+		return saleOrderLine;
+	}
+
+	public void setSaleOrderLines(ArrayList<SaleOrderLine> saleOrderLines) {
+		saleOrderLine = saleOrderLines;
+	}
+
+	public void setCustomerToCurrentSaleOrder(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Customer getCustomerFromSaleCurrentOrder() {
+		return customer;
+	}
+
+	public void setProductToCurrentSaleOrder(Product p) {
+		// Assuming you have a collection to store products in the current order
+		if (productsInOrder == null) {
+			productsInOrder = new ArrayList<>();
+		}
+		productsInOrder.add(p);
+	}
+	public ArrayList<Product> getProductsInSaleOrder() {
+        return productsInOrder;
+    }
+
 	
 	
 	
