69b3126d2f069c9fc349b5d02c6b583a8b4268b4
ShawnLohse <10478740@ucn.dk>
2023-10-12 10:42:39 +0200

    update customerctr

--- src/controller/CustomerCtr.java
+++ src/controller/CustomerCtr.java
@@ -1,5 +1,20 @@
 package controller;
 
+import java.sql.SQLException;
+
+import dblayer.DBCustomer;
+import dblayer.DBCustomerIF;
+import model.Customer;
+
 public class CustomerCtr {
+	private Customer customer;
+	private DBCustomerIF dBCustomerIF;
+	
+	public CustomerCtr() throws SQLException{
+		dBCustomerIF = new DBCustomer();
+	}
+	public Customer findCustomerById(int id) throws SQLException {
+		return dBCustomerIF.findCustomerById(id);
+	}
 
 }
