f9141f15b81687fc0005d502be0cc081c473570f
kimb0kastekniv <10197768@ucn.dk>
2023-10-12 14:14:28 +0200

    DBProduct update igen igen

--- src/dblayer/DBProduct.java
+++ src/dblayer/DBProduct.java
@@ -12,51 +12,46 @@ import model.GunReplica;
 import model.Product;
 
 public class DBProduct implements DBProductIF {
-	
-	private static final String selectAllQ = ""
-			+ "";
-	private static final String findByProductIdQ = selectAllQ + " where barcode = ?";
+
+	private static final String selectAllQ = "SELECT * FROM Product";
+	private static final String findByProductIdQ = selectAllQ + "WHERE product_Id = ?";
 	private PreparedStatement selectAll;
 	private PreparedStatement findByProductId;
-	
-	
+
 	public DBProduct() throws SQLException {
-		selectAll = DBConnection.getInstance().getConnection()
-				.prepareStatement(selectAllQ);
-		findByProductId = DBConnection.getInstance().getConnection()
-				.prepareStatement(findByProductIdQ);
+		selectAll = DBConnection.getInstance().getConnection().prepareStatement(selectAllQ);
+		findByProductId = DBConnection.getInstance().getConnection().prepareStatement(findByProductIdQ);
 	}
 
 	@Override
 	public Product findProductByBarcode(int barcode) throws SQLException {
 		try {
-			
+
 			findByProductId.setInt(1, barcode);
 			ResultSet rs = findByProductId.executeQuery();
 			Product p = null;
-			if(rs.next()) {
-				p = buildProduct(rs);	
+			if (rs.next()) {
+				p = buildProduct(rs);
 			}
 			return p;
 		} catch (SQLException e) {
-				throw e;
-			}	
+			e.printStackTrace();
+          return null;
 		}
+	}
 
 	public Product buildProduct(ResultSet rs) throws SQLException {
-		Product p = new Product(
-				rs.getString("name"),
-				rs.getDouble("purchasePrice"),
+		return new Product(
+				rs.getString("name"), 
+				rs.getDouble("purchasePrice"), 
 				rs.getDouble("salesPrice"),
-				rs.getInt("minStock"),
-				rs.getInt("product_Id"),
-				rs.getInt("supplier_Id"),
+				rs.getInt("minStock"), 
+				rs.getInt("product_Id"), 
+				rs.getInt("supplier_Id"), 
 				rs.getInt("country_Id"),
 				rs.getInt("type")
-				
-				
-			);
-		return p;
+
+		);
 	}
-	
+
 }
--- src/dblayer/DBProductIF.java
+++ src/dblayer/DBProductIF.java
@@ -1,5 +1,11 @@
 package dblayer;
 
+import java.sql.SQLException;
+
+import model.Product;
+
 public interface DBProductIF {
 
+	Product findProductByBarcode(int barcode) throws SQLException;
 }
+
