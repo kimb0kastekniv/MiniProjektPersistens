c2a63df3124f7ba4635e5702e5b498717a8eb515
ShawnLohse <10478740@ucn.dk>
2023-10-12 11:12:14 +0200

    Update ProductController

--- src/controller/ProductCtr.java
+++ src/controller/ProductCtr.java
@@ -1,7 +1,22 @@
 package controller;
 
+import java.sql.SQLException;
+
+import db.ProductDB;
+import db.ProductDBIF;
+import model.Product;
+
 public class ProductCtr {
-
-
+	private Product p;
+	private DBProductIF dBproductIF;
+	
+	public ProductCtr() throws SQLException {
+		dBproductIF = new ProductDB();
+	}
+	
+	public Product findProductByBarcode(int barcode) throws SQLException {
+		return dBproductIF.findProductByBarcode(barcode);
+	}
+	
 
 }
