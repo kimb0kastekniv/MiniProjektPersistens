284119988606050067c40bb593feee008584487b
ShawnLohse <10478740@ucn.dk>
2023-10-12 11:39:20 +0200

    Update Discount.java

--- src/model/Discount.java
+++ src/model/Discount.java
@@ -1,28 +1,27 @@
 package model;
 
 public class Discount {
-    // Define discount rules (you can expand this for more complex rules)
-    private static final double businessDiscountRate = 0.10;
-    private static final double privateDiscountAmount = 45;
+	private static final double businessDiscountRate = 0.10;
+	private static final double privateDiscountAmount = 45;
 
-    public double getAmountWithDiscount(String customerType, double saleOrderAmount) {
-        if (customerType.equalsIgnoreCase("BusinessCustomer")) {
-            if (saleOrderAmount >= 2500) {
-                return applyBusinessDiscount(saleOrderAmount);
-            }
-        } else if (customerType.equalsIgnoreCase("private")) {
-            if (saleOrderAmount >= 1500) {
-                return applyPrivateDiscount(saleOrderAmount);
-            }
-        }
-        return saleOrderAmount; // No applicable discount
-    }
+	public double getAmountWithDiscount(int customerType, double saleOrderAmount) {
+		if (customerType == 2) { // Use 2 for "Business" customer
+			if (saleOrderAmount >= 2500) {
+				return applyBusinessDiscount(saleOrderAmount);
+			}
+		} else if (customerType == 1) { // Use 1 for "private" customer type
+			if (saleOrderAmount >= 1500) {
+				return applyPrivateDiscount(saleOrderAmount);
+			}
+		}
+		return saleOrderAmount; 
+	}
 
-    private double applyBusinessDiscount(double amount) {
-        return amount - (amount * businessDiscountRate);
-    }
+	private double applyBusinessDiscount(double amount) {
+		return amount - (amount * businessDiscountRate);
+	}
 
-    private double applyPrivateDiscount(double amount) {
-        return amount - privateDiscountAmount;
-    }
+	private double applyPrivateDiscount(double amount) {
+		return amount - privateDiscountAmount;
+	}
 }
